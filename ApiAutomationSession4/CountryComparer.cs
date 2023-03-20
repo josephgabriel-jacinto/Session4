0c0b3c5b3e5fa12bc5ea4710735926a17c48f4f3
Joseph Gabriel Jacinto <2041504@cognizant.com>
2023-03-21 02:07:56 +0800

    Final Commit

---
+++ ApiAutomationSession4/CountryComparer.cs
@@ -0,0 +1,23 @@
+﻿using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAutomationSession4
+{
+    public class CountryComparer : IEqualityComparer<ServiceReference1.tCountryCodeAndName>
+    {
+        public bool Equals(tCountryCodeAndName? x, tCountryCodeAndName? y)
+        {
+            return x.sISOCode == y.sISOCode;
+        }
+
+        public int GetHashCode(tCountryCodeAndName obj)
+        {
+            return obj.sISOCode.GetHashCode();
+        }
+    }
+}
---
+++ ApiAutomationSession4/CountryInfoTests.cs
@@ -0,0 +1,56 @@
+﻿using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAutomationSession4
+{
+    [TestClass]
+    public class CountryInfoTests
+    {
+        private readonly ServiceReference1.CountryInfoServiceSoapTypeClient countryInfoClient = new ServiceReference1.CountryInfoServiceSoapTypeClient(ServiceReference1.CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+
+        [TestMethod]
+        public void ValidateAscendingOrderByCountryCode()
+        {
+            var listOfCountry = countryInfoClient.ListOfCountryNamesByCode().ToList();
+            var orderedListOfCountry = countryInfoClient.ListOfCountryNamesByCode().ToList();
+
+            //order by ascending by sISOCode
+            orderedListOfCountry.Sort((x, y) => x.sISOCode.CompareTo(y.sISOCode));
+            
+            Assert.IsTrue(Enumerable.SequenceEqual(orderedListOfCountry, listOfCountry, new CountryComparer()), "List of country mismatched");
+        }
+
+
+        [TestMethod]
+        public void ValidateInvalidCountryCode()
+        {
+            var getResponse = countryInfoClient.CountryName("XXX");
+            Assert.AreEqual("Country not found in the database", getResponse, "Incorrect response for Invalid Country sISOCode");
+        }
+
+
+        [TestMethod]
+        public void ValidateCountryNameByLastCountryCode()
+        {
+            //get list of countries
+            var countryList = countryInfoClient.ListOfCountryNamesByCode();
+
+            //get last entry
+            var lastCountry = countryList.Last();
+            string lastCountryCode = lastCountry.sISOCode;
+
+            //use last entry code to get country name
+            var countryName = countryInfoClient.CountryName(lastCountryCode);
+
+            Assert.AreEqual(lastCountry.sName, countryName, "Country name mismatched");
+        }
+
+    }
+}
